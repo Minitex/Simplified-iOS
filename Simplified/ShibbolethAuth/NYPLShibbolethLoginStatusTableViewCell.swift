a5ca91b1a9f946e319579388fb4c0aa70e16fd0d
Vui Nguyen <vui_engineer@yahoo.com>
2019-02-26 12:30:17 -0700

    make login status cell match styling of existing table cells

--- Simplified/ShibbolethAuth/NYPLShibbolethLoginStatusTableViewCell.swift
+++ Simplified/ShibbolethAuth/NYPLShibbolethLoginStatusTableViewCell.swift
@@ -7,13 +7,18 @@
 //
 
 import UIKit
+import PureLayout
 
 class NYPLShibbolethLoginStatusTableViewCell: UITableViewCell {
 
+  @IBOutlet weak var statusTextField: UITextField!
+  let verticalMarginPadding = 2.0
+  
   override func awakeFromNib() {
     super.awakeFromNib()
     // Initialization code
     selectionStyle = UITableViewCellSelectionStyle.none
+    layoutTextField()
   }
 
   override func setSelected(_ selected: Bool, animated: Bool) {
@@ -22,4 +27,16 @@ class NYPLShibbolethLoginStatusTableViewCell: UITableViewCell {
     // Configure the view for the selected state
   }
 
+  func layoutTextField() {
+    statusTextField.font = UIFont.customFont(forTextStyle: UIFontTextStyle.body)
+    statusTextField.text = "We are logged in!!!"
+    statusTextField.preservesSuperviewLayoutMargins = true
+    statusTextField.autoPinEdge(toSuperviewMargin: ALEdge.right)
+    statusTextField.autoPinEdge(toSuperviewMargin: ALEdge.left)
+
+    // PureLayout
+    statusTextField.autoConstrainAttribute(ALAttribute.top, to: ALAttribute.marginTop, of: statusTextField.superview!, withOffset: CGFloat(verticalMarginPadding))
+    statusTextField.autoConstrainAttribute(ALAttribute.bottom, to: ALAttribute.marginBottom, of: statusTextField.superview!, withOffset: CGFloat(-verticalMarginPadding))
+
+  }
 }
